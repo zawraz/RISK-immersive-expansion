f3568576aeb14970ade0985304c9efdd31887fe1
Konrad Sawras <zawraz@gmail.com>
2023-09-14 09:48:19 +0200

    prettier-plugin-tailwindcss integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello World!</h1>
+  return (
+    <h1 className="px-9 py-8 text-3xl font-bold underline">Hello World!</h1>
+  );
 }
-export default App
+export default App;
