28abee063f2ae11d78f96f3f5ba5e7a92d87a8eb
Konrad Sawras <zawraz@gmail.com>
2023-10-12 09:33:03 +0200

    Update App.tsx
    Prettier for tailwindcss validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1 className="px-9 py-8 text-3xl font-bold underline">Hello World!</h1>
+    <h1 className="bg-amber-200 px-9 py-8 text-3xl font-bold underline">
+      Hello World!
+    </h1>
   );
 }
 export default App;
